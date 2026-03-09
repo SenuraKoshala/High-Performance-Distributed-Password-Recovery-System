4d32f7c5fb4b2c9d6b37b005188e06405bd55543
senurakoshala <koshala19299@gmail.com>
2026-03-09 10:08:49 +0530

    Initial commit

---
+++ src/serial_password_cracker.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <iomanip>
+#include <openssl/md5.h>
+
+using namespace std;
+using namespace chrono;
+
+const string CHARSET   = "abcdefghijklmnopqrstuvwxyz0123456789";
+const int    MAX_LENGTH = 5;   // Maximum password length to try
+const int    CHARSET_SIZE = CHARSET.size();
+
+
+int main() {
+
+
+    return 0;
+}
